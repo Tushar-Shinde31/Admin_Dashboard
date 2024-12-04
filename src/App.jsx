957cdde9986abb7d19706a113aedd18dfae1451c
Tushar-Shinde45 <149402930+Tushar-Shinde45@users.noreply.github.com>
2024-12-04 18:54:51 +0530

    build the sidebar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProductPage from "./pages/ProductPage";
+import Sidebar from "./components/Sidebar";
+
+function App () {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80"></div>
+      </div>
+
+      <Sidebar/>
+      <Routes>
+        <Route path="/" element={<OverviewPage/>}></Route>
+        <Route path="/product" element={<ProductPage/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
